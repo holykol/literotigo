e50bcdfd1d5ec2d040b27001c4740cf04c8687a6
Timofey <ouxya@pm.me>
2021-04-26 22:35:25 +0300

    v0.2

--- main.go
+++ main.go
@@ -1,27 +1,35 @@
 package main
 
 // TODO build index, searching by tags, author and mb even text
-// TODO parallel indexing testing navigation nix package
+// TODO parallel indexing testing nix package
 // Goal: low memory consumption. ideally less than < 20 megs
-// Goal: Single 20loc mmap dependency
+// Goal: Single ~20 loc mmap dependency
 
 import (
 	"bytes"
+	"embed"
 	_ "embed"
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"runtime"
 	"strconv"
 	"time"
 
 	"github.com/edsrzf/mmap-go"
+
+	_ "net/http/pprof"
 )
 
-//go:embed index.html
-var html string
+const entriesCount = 473653 // cat source.jsonl | wc -l
+
+//go:embed *.html
+var views embed.FS
 
 var funcs = template.FuncMap{
 	"add": func(a, b int) int {
@@ -29,8 +37,28 @@ var funcs = template.FuncMap{
 	},
 }
 
+type meta struct {
+	ID       int
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Category string `json:"category"`
+}
+
+type entry struct {
+	Text template.HTML `json:"text"`
+	Meta meta          `json:"meta"`
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	if len(os.Args) < 2 || os.Args[1] == "" {
+		fmt.Println("usage: literotigo database.jsonl")
+		return
+	}
+
 	log.Printf("Starting literotigo. Opening %s", os.Args[1])
+
 	file, err := os.Open(os.Args[1])
 	check(err)
 
@@ -41,7 +69,10 @@ func main() {
 	r := bytes.NewBuffer(mmap)
 	n := 0
 
-	indices := make([]int64, 0, 473653)
+	indices := make([]int, 0)
+	titles := make(map[int]string)
+	authors := make(map[string][]int)
+	categories := make(map[string][]int)
 
 	{
 		log.Println("Started indexing")
@@ -52,35 +83,97 @@ func main() {
 			if err == io.EOF {
 				break
 			}
+
+			var res struct {
+				Meta meta `json:"meta"`
+			}
+
+			err = json.Unmarshal([]byte(l), &res)
 			check(err)
 
-			indices = append(indices, int64(n))
+			if res.Meta.Category == "" {
+				fmt.Println("empty")
+			}
+
+			titles[i] = res.Meta.Title
+			authors[res.Meta.Author] = append(authors[res.Meta.Author], i)
+			categories[res.Meta.Category] = append(categories[res.Meta.Category], i)
+
+			indices = append(indices, n)
 			n += len(l)
 		}
 
 		log.Printf("Finished indexing in %f seconds", time.Now().Sub(start).Seconds())
+
+		runtime.GC()
 	}
 
-	tmpl := template.Must(template.New("").Funcs(funcs).Parse(html))
+	tmpl := template.Must(template.New("").Funcs(funcs).ParseFS(views, "*"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := make(map[string]interface{})
+
 		type entry struct {
-			Text template.HTML `json:"text"`
-			Meta struct {
-				ID       int
-				Title    string `json:"title"`
-				Author   string `json:"author"`
-				Category string `json:"category"`
-			} `json:"meta"`
+			ID    int
+			Title string
 		}
 
-		var id int
-		if n := r.URL.Query().Get("n"); n != "" {
-			id, err = strconv.Atoi(n)
-			if err != nil {
-				http.Error(w, "error while parsing ", 500)
-				return
+		// 20 random
+		{
+			list := make([]entry, 20)
+
+			for i := range list {
+				id := rand.Intn(len(indices))
+				title := titles[id]
+
+				list[i] = entry{id, title}
 			}
+
+			data["title"] = "20 Random"
+			data["list"] = list
+		}
+
+		if tag := r.URL.Query().Get("tag"); tag != "" {
+			list := make([]entry, 0)
+
+			for _, id := range categories[tag] {
+				list = append(list, entry{id, titles[id]})
+			}
+
+			data["title"] = tag
+			data["list"] = list
+		}
+
+		if author := r.URL.Query().Get("author"); author != "" {
+			list := make([]entry, 0)
+
+			for _, id := range authors[author] {
+				list = append(list, entry{id, titles[id]})
+			}
+
+			data["title"] = "From " + author
+			data["list"] = list
+		}
+
+		data["tags"] = categories // all tags
+
+		if err := tmpl.ExecuteTemplate(w, "index.html", data); err != nil {
+			renderError(w, err, "error executing template")
+			return
+		}
+	})
+
+	http.HandleFunc("/view", func(w http.ResponseWriter, r *http.Request) {
+		n := r.URL.Query().Get("id")
+		if n == "" {
+			renderError(w, nil, "missing id param")
+			return
+		}
+
+		id, err := strconv.ParseUint(n, 10, 64)
+		if err != nil {
+			renderError(w, err, "error while parsing query")
+			return
 		}
 
 		start := indices[id]
@@ -88,18 +181,24 @@ func main() {
 
 		var res entry
 		if err := json.Unmarshal(mmap[start:end], &res); err != nil {
-			http.Error(w, "error while parsing json", 500)
+			renderError(w, err, "error while parsing json")
 			return
 		}
-		res.Meta.ID = id
+		res.Meta.ID = int(id)
 
-		if err := tmpl.Execute(w, res); err != nil {
-			http.Error(w, "error executing template", 500)
-			return
+		if err := tmpl.ExecuteTemplate(w, "view.html", res); err != nil {
+			renderError(w, err, "error executing template")
 		}
 	})
 
+	log.Println("Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func renderError(w http.ResponseWriter, err error, s string) {
+	e := fmt.Sprintf("%s: %v", s, err)
+	log.Printf(e)
+	http.Error(w, e, 500)
 }
 
 func check(err error) {
